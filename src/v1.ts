fc486bd4e6051850e659213052464696d80706cd
White Autumn <white@autumn.dev>
2022-09-29 14:26:32 +0200

    Refactor parsePackages function

--- src/v1.ts
+++ src/v1.ts
@@ -12,35 +12,35 @@ type InternalParsedPackage = ParsedPackage & {
 type RawDependencies = Record<string, RawPackageV1>;
 type ParsedDependencies = Record<string, ParsedPackage>;
 
-export const parse = (raw: RawLockfileV1, pkg: PackageJson): ParsedLockfile => {
+const parsePackages = (packageNames: string[], rawPackages: RawDependencies, parsedPackages: ParsedDependencies) => {
+	for (const packageName of packageNames) {
+		
+		const rawPackage = rawPackages[packageName];
+		if (rawPackage == null) {
+			throw new Error('Given package.json and package-lock.json files are out of sync!');
+		}
+
+		const [supported, unsupported] = pick(rawPackage, 'version');
+		const parsedPackage: ParsedPackage = {
+			version: supported.version
+		};
+
+		(<InternalParsedPackage> parsedPackage)[INTERNAL] = {
+			unsupported
+		};
+
+		parsedPackages[packageName] = parsedPackage;
+	}
+};
+
+export const parse = (raw: RawLockfileV1, packagefile: PackageJson): ParsedLockfile => {
 	const parsed: ParsedLockfile = {
 		version: raw.lockfileVersion
 	};
 
-	const parsePackages = (packages: string[], input: RawDependencies, output: ParsedDependencies) => {
-		for (const name of packages) {
-			if (input[name] == null) {
-				// TODO: Consider if this scenario should throw?
-				continue;
-			}
-
-			const [supported, unsupported] = pick(input[name], 'version');
-
-			const parsedPackage: InternalParsedPackage = {
-				version: supported.version,
-
-				[INTERNAL]: {
-					unsupported
-				}
-			};
-
-			output[name] = <ParsedPackage> parsedPackage;
-		}
-	};
-
-	if (raw.dependencies != null && pkg.dependencies != null) {
+	if (raw.dependencies != null && packagefile.dependencies != null) {
 		parsed.dependencies = {};
-		parsePackages(Object.keys(pkg.dependencies), raw.dependencies, parsed.dependencies);
+		parsePackages(Object.keys(packagefile.dependencies), raw.dependencies, parsed.dependencies);
 	}
 
 	return parsed;
