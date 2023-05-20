054a595399d18d2bd7f32042c4ef3e4b50ebf33f
White Autumn <white@autumn.dev>
2023-05-20 12:00:51 +0200

    Apply formatting changes

--- resources/samples/index.ts
+++ resources/samples/index.ts
@@ -2,24 +2,24 @@ import fs from 'fs/promises';
 
 import index from './index.json';
 
-const variants = <const> ['v1', 'v2', 'v3'];
-const directories = <Directories[]> Object.keys(index);
+const variants = <const>['v1', 'v2', 'v3'];
+const directories = <Directories[]>Object.keys(index);
 
 type Variant = typeof variants[number];
 type Directories = keyof typeof index;
 
-const pendingLockfiles = <Record<Directories, Record<Variant, Promise<unknown>>>> {};
-const pendingPackageJsons = <Record<Directories, Promise<unknown>>> {};
+const pendingLockfiles = <Record<Directories, Record<Variant, Promise<unknown>>>>{};
+const pendingPackageJsons = <Record<Directories, Promise<unknown>>>{};
 
 for (const directory of directories) {
-  pendingLockfiles[directory] = <Record<Variant, Promise<unknown>>> {};
-  pendingPackageJsons[directory] = fs.readFile(`./resources/samples/${directory}/package.json`, 'utf8')
-    .then(JSON.parse);
+	pendingLockfiles[directory] = <Record<Variant, Promise<unknown>>>{};
+	pendingPackageJsons[directory] = fs.readFile(`./resources/samples/${directory}/package.json`, 'utf8')
+		.then(JSON.parse);
 
-  for (const variant of variants) {
-    pendingLockfiles[directory][variant] = fs.readFile(`./resources/samples/${directory}/package-lock.${variant}.json`, 'utf8')
-      .then(JSON.parse);
-  }
+	for (const variant of variants) {
+		pendingLockfiles[directory][variant] = fs.readFile(`./resources/samples/${directory}/package-lock.${variant}.json`, 'utf8')
+			.then(JSON.parse);
+	}
 }
 
 export const getPackageJson = (directory: Directories) => pendingPackageJsons[directory];
--- scripts/generate-lockfiles.mjs
+++ scripts/generate-lockfiles.mjs
@@ -16,79 +16,81 @@ const exec = util.promisify(child_process.exec);
 const exists = path => fs.access(path, fs.constants.F_OK).then(() => true).catch(() => false);
 
 try {
-  await exec('volta --version', { stdio: 'ignore' });
+	await exec('volta --version', { stdio: 'ignore' });
 }
 catch {
-  console.error('Volta is required to run this script. Please install Volta and try again.');
-  process.exit(1);
+	console.error('Volta is required to run this script. Please install Volta and try again.');
+	process.exit(1);
 }
 
 const samplesDir = path.join(
-  process.cwd(),
-  'resources',
-  'samples'
+	process.cwd(),
+	'resources',
+	'samples'
 );
 
 const workingDir = path.join(
-  os.tmpdir(),
-  "package-lock-parser"
+	os.tmpdir(),
+	"package-lock-parser"
 );
 
 const workingDirCreated = exists(workingDir)
-  .then(itExists => !itExists ? fs.mkdir(workingDir) : undefined);
+	.then(itExists => !itExists ? fs.mkdir(workingDir) : undefined);
 
 const sampleDirs = await fs.readdir(samplesDir, { withFileTypes: true }).then(it => it
-  .filter(it => it.isDirectory())
-  .map(it => it.name)
+	.filter(it => it.isDirectory())
+	.map(it => it.name)
 );
 
 await workingDirCreated;
 
 const packageFiles = await Promise.all(sampleDirs.map(sampleDir =>
-  fs.readFile(path.join(samplesDir, sampleDir, 'package.json'))
+	fs.readFile(path.join(samplesDir, sampleDir, 'package.json'))
 ));
 
 const inputData = sampleDirs.map((sampleDir, index) => ({
-  originalPath: path.join(samplesDir, sampleDir),
-  rawPackageFile: packageFiles[index]
+	originalPath: path.join(samplesDir, sampleDir),
+	rawPackageFile: packageFiles[index]
 }));
 
 const generateLockfile = async ({ originalPath, rawPackageFile }, lockfileVersion) => {
-  const dir = path.join(workingDir, crypto.randomUUID(), path.basename(originalPath));
-  await fs.mkdir(dir, { recursive: true });
-  await fs.writeFile(path.join(dir, 'package.json'), rawPackageFile);
+	const dir = path.join(workingDir, crypto.randomUUID(), path.basename(originalPath));
+	await fs.mkdir(dir, { recursive: true });
+	await fs.writeFile(path.join(dir, 'package.json'), rawPackageFile);
 
-  let npmVersion;
-  switch (lockfileVersion) {
-    case 1:
-      npmVersion = '6';
-      break;
-    case 2:
-      npmVersion = '8';
-      break;
-    case 3:
-      npmVersion = '9';
-      break;
-  }
+	let npmVersion;
+	switch (lockfileVersion) {
+		case 1:
+			npmVersion = '6';
+			break;
+		case 2:
+			npmVersion = '8';
+			break;
+		case 3:
+			npmVersion = '9';
+			break;
+	}
 
-  await exec(`npx npm@${npmVersion} install`, { cwd: dir, stdio: 'inherit', env: {
-    ...process.env,
-    npm_update_notifier: 'false',
-  } });
+	await exec(`npx npm@${npmVersion} install`, {
+		cwd: dir, stdio: 'inherit', env: {
+			...process.env,
+			npm_update_notifier: 'false',
+		}
+	});
 
-  const lockfile = await fs.readFile(path.join(dir, 'package-lock.json'));
-  await fs.writeFile(path.join(originalPath, `package-lock.v${lockfileVersion}.json`), lockfile);
+	const lockfile = await fs.readFile(path.join(dir, 'package-lock.json'));
+	await fs.writeFile(path.join(originalPath, `package-lock.v${lockfileVersion}.json`), lockfile);
 };
 
 const generateLockfiles = async (inputData) => {
-  await Promise.all([
-    limit(() => generateLockfile(inputData, 1)),
-    limit(() => generateLockfile(inputData, 2)),
-    limit(() => generateLockfile(inputData, 3))
-  ]);
+	await Promise.all([
+		limit(() => generateLockfile(inputData, 1)),
+		limit(() => generateLockfile(inputData, 2)),
+		limit(() => generateLockfile(inputData, 3))
+	]);
 };
 
 await Promise.all(inputData.map(generateLockfiles));
 
 const sampleDirIndex = Object.fromEntries(sampleDirs.map(dir => [dir, null]));
-await fs.writeFile(path.join(samplesDir, 'index.json'),JSON.stringify(sampleDirIndex, null, 2));
+await fs.writeFile(path.join(samplesDir, 'index.json'), JSON.stringify(sampleDirIndex, null, 2));
